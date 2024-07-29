560391eea432c7d42056f3b64b42cb7fcac98245
jeonseun <jeonseun1234@gmail.com>
2024-07-29 22:44:12 +0900

    chore: 의존성 추가
    - spring boot security & spring security test
    - spring boot jpa
    - MySQL jdbc driver
    - H2 jdbc driver

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,27 @@ repositories {
 }
 
 dependencies {
+    // spring boot web
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    // spring boot security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // spring boot jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // MySQL & H2 jdbc driver
+    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.h2database:h2")
+
+    // kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
